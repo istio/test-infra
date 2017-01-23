21a4a437272ad37e84487693626f1594c6dd0010
Sebastien Vas <sebastienvas@google.com>
2017-01-23 15:14:46 -0800

    Adds and update method for .bazelrc

--- src/org/istio/testutils/Bazel.groovy
+++ src/org/istio/testutils/Bazel.groovy
@@ -8,33 +8,37 @@ BAZEL_BUILD_ARGS = ''
 
 
 def setVars() {
-    BAZEL_ARGS = getWithDefault(env.BAZEL_ARGS)
-    BAZEL_BUILD_ARGS = getWithDefault(env.BAZEL_BUILD_ARGS)
+  BAZEL_ARGS = getWithDefault(env.BAZEL_ARGS)
+  BAZEL_BUILD_ARGS = getWithDefault(env.BAZEL_BUILD_ARGS)
 }
 
 def fetch(args) {
-    timeout(30) {
-        retry(3) {
-            sh("bazel ${BAZEL_ARGS} fetch ${args}")
-            sleep(5)
-        }
+  timeout(30) {
+    retry(3) {
+      sh("bazel ${BAZEL_ARGS} fetch ${args}")
+      sleep(5)
     }
+  }
 }
 
 def build(args) {
-    timeout(40) {
-        retry(3) {
-            sh("bazel ${BAZEL_ARGS} build ${BAZEL_BUILD_ARGS} ${args}")
-            sleep(5)
-        }
+  timeout(40) {
+    retry(3) {
+      sh("bazel ${BAZEL_ARGS} build ${BAZEL_BUILD_ARGS} ${args}")
+      sleep(5)
     }
+  }
 }
 
 def test(args) {
-    timeout(40) {
-        retry(3) {
-            sh("bazel ${BAZEL_ARGS} test ${args}")
-            sleep(5)
-        }
-    }
+  timeout(40) {
+    sh("bazel ${BAZEL_ARGS} test ${args}")
+    sleep(5)
+  }
+}
+
+def updateBazelRc(updateBazelrc='.bazelrc.jenkins') {
+  if (fileExists(updateBazelrc)) {
+    sh("cat ${updateBazelrc} >> .bazelrc")
+  }
 }
--- vars/goBuildNode.groovy
+++ vars/goBuildNode.groovy
@@ -4,14 +4,15 @@ Creates a node with the right label and checkout the source code.
 
 import org.istio.testutils.Utilities
 
-def call(gitUtils, buildPath, Closure body) {
+
+def call(gitUtils, goImportPath, Closure body) {
   utils = new Utilities()
   def nodeLabel = utils.getParam('SLAVE_LABEL', gitUtils.DEFAULT_SLAVE_LABEL)
   def buildNodeLabel = "${nodeLabel}-build"
   node(buildNodeLabel) {
     env.GOPATH = env.WORKSPACE
     env.PATH = "${env.GOPATH}/bin:${env.PATH}"
-    def newWorkspace = "${env.GOPATH}/src/${buildPath}"
+    def newWorkspace = "${env.GOPATH}/src/${goImportPath}"
     sh("mkdir -p ${newWorkspace}")
     dir(newWorkspace) {
       gitUtils.checkoutSourceCode()
