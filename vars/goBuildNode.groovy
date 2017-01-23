e6a23d96f6baf84317b0e2ce83dafe6bcaf9d01f
Sebastien Vas <sebastienvas@google.com>
2017-01-23 13:22:02 -0800

    Added a build node for golang

---
+++ vars/goBuildNode.groovy
@@ -0,0 +1,21 @@
+/*
+Creates a node with the right label and checkout the source code.
+*/
+
+import org.istio.testutils.Utilities
+
+def call(gitUtils, buildPath, Closure body) {
+  utils = new Utilities()
+  def nodeLabel = utils.getParam('SLAVE_LABEL', gitUtils.DEFAULT_SLAVE_LABEL)
+  def buildNodeLabel = "${nodeLabel}-build"
+  node(buildNodeLabel) {
+    env.GOPATH = env.WORKSPACE
+    env.PATH = "${env.GOPATH}/bin:${env.PATH}"
+    def newWorkspace = "${env.GOPATH}/src/${buildPath}"
+    sh("mkdir -p ${newWorkspace}")
+    dir(newWorkspace) {
+      gitUtils.checkoutSourceCode()
+      body()
+    }
+  }
+}
