e1b3dd7502cf55ae5790a4257487fe8927239878
Sebastien Vas <sebastienvas@google.com>
2017-01-27 12:50:33 -0800

    Fixes typo

--- vars/buildNode.groovy
+++ vars/buildNode.groovy
@@ -4,7 +4,7 @@ Creates a node with the right label and checkout the source code.
 
 def call(gitUtils, Closure body) {
   def nodeLabel = params.get('SLAVE_LABEL')
-  if (nodelabel == '') {
+  if (nodeLabel == '') {
     nodeLabel = gitUtils.DEFAULT_SLAVE_LABEL
   }
   def buildNodeLabel = "${nodeLabel}-build"
