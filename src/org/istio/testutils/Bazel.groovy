a37e5f0c2e321cc9c2a7c9af768105be68bd8086
Sebastien Vas <sebastienvas@users.noreply.github.com>
2017-02-08 16:52:01 -0800

    Allow overwritting of Bazel env (#28)

--- src/org/istio/testutils/Bazel.groovy
+++ src/org/istio/testutils/Bazel.groovy
@@ -1,18 +1,24 @@
 package org.istio.testutils
 
-BAZEL_ARGS = ''
+BAZEL_STARTUP_ARGS = ''
 BAZEL_BUILD_ARGS = ''
+BAZEL_TEST_ARGS = ''
 
+def defaultValue(first, second) {
+  def value = first == null ? second : first
+  return value == null ? '' : value
+}
 
-def setVars() {
-  BAZEL_ARGS = env.BAZEL_ARGS
-  BAZEL_BUILD_ARGS = env.BAZEL_BUILD_ARGS
+def setVars(startup = null, build = null, test = null) {
+  BAZEL_STARTUP_ARGS = defaultValue(startup, env.BAZEL_ARGS)
+  BAZEL_BUILD_ARGS = defaultValue(build, env.BAZEL_BUILD_ARGS)
+  BAZEL_TEST_ARGS = defaultValue(test, env.BAZEL_TEST_ARGS)
 }
 
 def fetch(args) {
   timeout(30) {
     retry(3) {
-      sh("bazel ${BAZEL_ARGS} fetch ${args}")
+      sh("bazel ${BAZEL_STARTUP_ARGS} fetch ${args}")
       sleep(5)
     }
   }
@@ -21,7 +27,7 @@ def fetch(args) {
 def build(args) {
   timeout(40) {
     retry(3) {
-      sh("bazel ${BAZEL_ARGS} build ${BAZEL_BUILD_ARGS} ${args}")
+      sh("bazel ${BAZEL_STARTUP_ARGS} build ${BAZEL_BUILD_ARGS} ${args}")
       sleep(5)
     }
   }
@@ -29,7 +35,7 @@ def build(args) {
 
 def test(args) {
   timeout(40) {
-    sh("bazel ${BAZEL_ARGS} test ${args}")
+    sh("bazel ${BAZEL_STARTUP_ARGS} test ${BAZEL_TEST_ARGS} ${args}")
     sleep(5)
   }
 }
@@ -38,7 +44,7 @@ def version() {
   sh('bazel version')
 }
 
-def updateBazelRc(updateBazelrc='.bazelrc.jenkins') {
+def updateBazelRc(updateBazelrc = '.bazelrc.jenkins') {
   if (fileExists(updateBazelrc)) {
     sh("cat ${updateBazelrc} >> .bazelrc")
   }
