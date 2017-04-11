96c1bdb12710c3aea44fb0a2242203bcb8ccf70e
Sebastien Vas <sebastienvas@users.noreply.github.com>
2017-04-10 20:14:42 -0700

    Use env variable instead of global variable. (#172)
    Update codecov

--- vars/buildNode.groovy
+++ vars/buildNode.groovy
@@ -5,7 +5,7 @@ Creates a node with the right label and checkout the source code.
 def call(gitUtils, Closure body) {
   def nodeLabel = params.get('SLAVE_LABEL')
   if (nodeLabel == null) {
-    nodeLabel = gitUtils.DEFAULT_SLAVE_LABEL
+    nodeLabel = env.DEFAULT_SLAVE_LABEL
   }
   def buildNodeLabel = "${nodeLabel}-build"
   node(buildNodeLabel) {
