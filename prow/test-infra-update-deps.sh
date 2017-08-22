6ed49d40afe07682c869d10f9b67b5e67686db92
Charles Xu <chx@google.com>
2017-08-22 11:28:34 -0700

    Periodic Prow Job to Update Istio Dependency SHA (#400)
    Automatic merge from submit-queue
    
    Periodic Prow Job to Update Istio Dependency SHA
    
    An example PR created is https://github.com/istio/istio/pull/588
    
    Notice that it reads SHA from stable branch of each dependency. For the entire dependency update scheme to be automated, it also needs the fast-forward jobs that move stable up-to-date with master, which will be done in another PR shortly after.
    
    **Release note**:
    
    ```release-note
    NONE
    ```

---
+++ prow/test-infra-update-deps.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Copyright 2017 Istio Authors
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+#######################################################
+# Crontab dependency update script triggered by Prow. #
+#######################################################
+
+# Exit immediately for non zero status
+set -e
+# Check unset variables
+set -u
+# Print commands
+set -x
+
+echo "=== Building Binary that Updates Istio Dependency ==="
+bazel build //toolbox/deps_update:update_deps
+
+git config --global user.email "istio.testing@gmail.com"
+git config --global user.name "istio-bot"
+
+echo "=== Updating Dependency of Istio ==="
+./bazel-bin/toolbox/deps_update/update_deps \
+--repo=istio \
+--token_file=/etc/github/oauth \
+--hub=gcr.io/istio-testing
