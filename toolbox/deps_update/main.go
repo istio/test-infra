fb8cce0a5bba949fc12bfa5d3b647d12ea722e28
Krishna Pagadala <rkrishnap@google.com>
2018-01-05 17:52:41 -0800

    change path for checkout (#656)
    Automatic merge from submit-queue.
    
    change path for checkout

--- toolbox/deps_update/main.go
+++ toolbox/deps_update/main.go
@@ -126,7 +126,7 @@ func updateDependenciesOf(repo string) error {
 	if err != nil {
 		return err
 	}
-	repoDir, err := u.CloneRepoCheckoutBranch(githubClnt, repo, *baseBranch, "", "src/istio.io")
+	repoDir, err := u.CloneRepoCheckoutBranch(githubClnt, repo, *baseBranch, "", "go/src/istio.io")
 	if err != nil {
 		return err
 	}
@@ -134,7 +134,7 @@ func updateDependenciesOf(repo string) error {
 		if err = os.Chdir(saveDir); err != nil {
 			log.Fatalf("Error during chdir: %v\n", err)
 		}
-		if err = u.RemoveLocalRepo("src"); err != nil {
+		if err = u.RemoveLocalRepo("go"); err != nil {
 			log.Fatalf("Error during clean up: %v\n", err)
 		}
 	}()
@@ -175,7 +175,7 @@ func updateDependenciesOf(repo string) error {
 	if repo == istioRepo {
 		goPath := path.Join(repoDir, "../../..")
 		env := "GOPATH=" + goPath
-		if _, err = u.Shell(env + " make depend"); err != nil {
+		if _, err = u.Shell(env + " make depend.update"); err != nil {
 			return err
 		}
 	}
@@ -215,7 +215,7 @@ func init() {
 func main() {
 	if *repo != "" { // only update dependencies of this repo
 		if err := updateDependenciesOf(*repo); err != nil {
-			log.Fatalf("Failed to udpate dependency: %v\n", err)
+			log.Fatalf("Failed to update dependency: %v\n", err)
 		}
 	} else { // update dependencies of all repos in the istio project
 		repos, err := githubClnt.ListRepos()
@@ -225,7 +225,7 @@ func main() {
 		}
 		for _, r := range repos {
 			if err := updateDependenciesOf(r); err != nil {
-				log.Fatalf("Failed to udpate dependency: %v\n", err)
+				log.Fatalf("Failed to update dependency: %v\n", err)
 			}
 		}
 	}
