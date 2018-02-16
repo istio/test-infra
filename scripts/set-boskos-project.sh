655fa30fc130748050d5a27eae9bda07a8ddbe0a
Sebastien Vas <sebastienvas@google.com>
2018-02-16 11:24:08 -0800

    Adds documentation

---
+++ scripts/set-boskos-project.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+DIR="${ROOT}/scripts"
+
+. ${DIR}/all-utilities || { echo "Cannot load Bash utilities" ; exit 1 ; }
+
+# Exit immediately for non zero status
+set -e
+# Check unset variables
+set -u
+# Print commands
+set -x
+
+USERS=()
+USERS+=('serviceAccount:boskos@istio-testing.iam.gserviceaccount.com')
+USERS+=('serviceAccount:istio-prow-test-job@istio-testing.iam.gserviceaccount.com')
+USERS+=('group:mdb.istio-testing@google.com')
+
+SERVICES=()
+SERVICES+=(compute.googleapis.com)
+SERVICES+=(container.googleapis.com)
+
+while getopts :p:c arg; do
+  case ${arg} in
+    p) PROJECT_ID="${OPTARG}";;
+    *) error_exit "Unrecognized argument -${OPTARG}";;
+  esac
+done
+
+for sa in ${USERS[@]}; do
+  gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=${sa} --role roles/owner
+done
+
+for s in ${SERVICES[@]}; do
+  gcloud services enable ${s} --project ${PROJECT_ID}
+done
+
