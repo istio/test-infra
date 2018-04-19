7fd6ff69b4d6beba4ee5a9f42707b3e94264dd6e
Sebastien Vas <sebastienvas@google.com>
2018-04-19 11:38:57 -0700

    Attempting to make plugins test work

--- prow/config/config_test.go
+++ prow/config/config_test.go
@@ -19,6 +19,7 @@ import (
 
 	"k8s.io/test-infra/prow/config"
 	"k8s.io/test-infra/prow/plugins"
+	_ "k8s.io/test-infra/prow/hook"
 )
 
 func TestConfig(t *testing.T) {
@@ -30,7 +31,6 @@ func TestConfig(t *testing.T) {
 
 // Make sure that our plugins are valid.
 func TestPlugins(t *testing.T) {
-	t.Skip("unable to load plugins help for whatever reason")
 	pa := &plugins.PluginAgent{}
 	if err := pa.Load("../plugins.yaml"); err != nil {
 		t.Fatalf("could not load plugins: %v", err)
