cf07aa5d58e62c8f459199e6e248f4952ef02c95
Krishna Pagadala <rkrishnap@google.com>
2018-05-01 21:56:25 +0000

    add git_branches for green build and update bot

--- prow/test-infra-update-deps.sh
+++ prow/test-infra-update-deps.sh
@@ -28,51 +28,64 @@ set -x
 
 echo "=== Building Binary that Updates Istio Dependency ==="
 bazel build //toolbox/deps_update:deps_update
+UPDATE_BINARY="./bazel-bin/toolbox/deps_update/linux_amd64_stripped/deps_update"
 
 git config --global user.email "istio.testing@gmail.com"
 git config --global user.name "istio-bot"
 
 TOKEN_PATH="/etc/github/oauth"
-UPDATE_EXT_DEP="false"
 
-UPDATE_BINARY="./bazel-bin/toolbox/deps_update/linux_amd64_stripped/deps_update"
 
-# List of repo where auto dependency update has been enabled
-# excluding istio/istio
-case ${GIT_BRANCH} in
-  master)
-    hour=`date "+%I"` #( 1..12)
-    day_of_week=`date "+%u"` #( 1..7)
-    repos=( proxy )
-    hour=25 #disable bot for istio repo, its broken now
-    # TODO need to update the bot for istio after fixing it
-    case ${hour} in
-      02|04|06|08|10|12)
-	${UPDATE_BINARY} \
-		--repo="istio" \
-		--base_branch=${GIT_BRANCH} \
+function update_on_branch {
+   local CUR_BRANCH=$1
+
+   UPDATE_EXT_DEP="false"
+   # List of repo where auto dependency update has been enabled
+   # excluding istio/istio
+   case ${CUR_BRANCH} in
+     master|release-*)
+       hour=`date "+%I"` #( 1..12)
+       day_of_week=`date "+%u"` #( 1..7)
+       hour=25 #disable bot for istio repo, its broken now
+       # TODO need to update the bot for istio after fixing it
+       case ${hour} in
+         02|04|06|08|10|12)
+	   ${UPDATE_BINARY} \
+	   	--repo="istio" \
+	   	--base_branch=${CUR_BRANCH} \
 		--token_file=${TOKEN_PATH} \
 		--update_ext_dep="false"
-        ;;
-      *)
-        ;;
-    esac
-    hour24=`date "+%k"` #( 0..23)
-    if [ "${hour24}" -ge 20 ] && [ "${day_of_week}" -eq 2 ]; then
-	# external deps (envoyproxy in proxy and depend.update in istio) updated only once a week
-        UPDATE_EXT_DEP="true"
-    fi
-    ;;
-  *)
-    echo error GIT_BRANCH set incorrectly; exit 1
-    ;;
-esac
+           ;;
+         *)
+           ;;
+       esac
+       hour24=`date "+%k"` #( 0..23)
+       if [ "${hour24}" -ge 20 ] && [ "${day_of_week}" -eq 2 ]; then
+   	   # external deps (envoyproxy in proxy and depend.update in istio) updated only once a week
+           UPDATE_EXT_DEP="true"
+       fi
+       ;;
+     *)
+       echo error CUR_BRANCH:$CUR_BRANCH, all branches:$GIT_BRANCHES set incorrectly; exit 1
+       ;;
+   esac
 
-for r in "${repos[@]}"; do
-  echo "=== Updating Dependency of ${r} ==="
-  ${UPDATE_BINARY} \
-    --repo=${r} \
-    --base_branch=${GIT_BRANCH} \
-    --token_file=${TOKEN_PATH} \
-    --update_ext_dep=${UPDATE_EXT_DEP}
+   repos=( proxy )
+   for r in "${repos[@]}"; do
+     echo "=== Updating Dependency of ${r} ==="
+     ${UPDATE_BINARY} \
+       --repo=${r} \
+       --base_branch=${CUR_BRANCH} \
+       --token_file=${TOKEN_PATH} \
+       --update_ext_dep=${UPDATE_EXT_DEP}
+   done
+}
+
+IFS=,
+branches=($GIT_BRANCHES)
+unset IFS
+
+for branch in "${branches[@]}"; do
+    update_on_branch $branch
 done
+
