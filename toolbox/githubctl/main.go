9901f878f6975fcff42dbb64d96a0fcce5349605
Krishna Pagadala <rkrishnap@google.com>
2018-06-25 19:33:56 +0000

    fix postSubmitJobsMap

--- toolbox/githubctl/main.go
+++ toolbox/githubctl/main.go
@@ -47,18 +47,22 @@ var (
 	// needs to be consistent with prow config map
 	postSubmitJobsMap = map[string][]string{
 	 "master": []string{
+		"e2e-mixer-no_auth",
+		"e2e-bookInfoTests-envoyv2-v1alpha3",
+		"istio-pilot-e2e-envoyv2-v1alpha3",
+		"e2e-simpleTests",
+		"e2e-dashboard",
 		"istio-postsubmit",
-		"e2e-suite-rbac-no_auth",
-		"e2e-suite-rbac-auth",
-		"e2e-cluster_wide-auth",
-		},
-	 "release-0.8": []string{
-		"istio-postsubmit",
-		"e2e-suite-rbac-no_auth",
-		"e2e-suite-rbac-auth",
-		"e2e-cluster_wide-auth",
 		},
 	 "release-1.0.0-snapshot-0": []string{
+		"e2e-mixer-no_auth",
+		"e2e-bookInfoTests-envoyv2-v1alpha3",
+		"istio-pilot-e2e-envoyv2-v1alpha3",
+		"e2e-simpleTests",
+		"e2e-dashboard",
+		"istio-postsubmit",
+		},
+	 "release-0.8": []string{
 		"istio-postsubmit",
 		"e2e-suite-rbac-no_auth",
 		"e2e-suite-rbac-auth",
