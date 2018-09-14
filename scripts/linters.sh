1eda9d0cf9e6f94418fc80c2240bd75c7ab7f64b
Sebastien Vas <sebastienvas@google.com>
2018-09-13 22:52:57 -0700

    Update presubmits with pod-utils

--- scripts/linters.sh
+++ scripts/linters.sh
@@ -3,46 +3,67 @@ set -e
 
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 BIN_PATH="${ROOT}/scripts"
+UNSET_GOPATH="$(cd "$(dirname "${ROOT})")/../.." && pwd)"
+GOPATH=${GOPATH:-${UNSET_GOPATH}}
 
-echo 'Installing gometalinter ...'
-go get -u gopkg.in/alecthomas/gometalinter.v2
+function install_gometalinter() {
+  echo 'Installing gometalinter ...'
+  go get -u gopkg.in/alecthomas/gometalinter.v2
+  "${GOPATH}/bin/gometalinter.v2" --install
+}
 
-gometalinter.v2 --install
+function run_gometalinter() {
+  echo 'Running gometalinter ...'
+  "${GOPATH}/bin/gometalinter.v2" ./... \
+    --concurrency=4\
+    --deadline=600s --disable-all\
+    --enable=deadcode\
+    --enable=errcheck\
+    --enable-gc\
+    --enable=goconst\
+    --enable=gofmt\
+    --enable=goimports\
+    --enable=golint --min-confidence=0\
+    --enable=ineffassign\
+    --enable=interfacer\
+    --enable=lll --line-length=160\
+    --enable=megacheck\
+    --enable=misspell\
+    --enable=structcheck\
+    --enable=unconvert\
+    --enable=varcheck\
+    --enable=vet\
+    --enable=vetshadow\
+    --exclude='should have a package comment'\
+    --exclude=.pb.go\
+    --vendor\
+    --vendored-linters\
+    ./...
+  echo 'gometalinter OK'
+}
 
-cd ${ROOT}
-echo 'Running gometalinter ...'
-gometalinter.v2 ./... \
-  --concurrency=4\
-  --deadline=600s --disable-all\
-  --enable=deadcode\
-  --enable=errcheck\
-  --enable-gc\
-  --enable=goconst\
-  --enable=gofmt\
-  --enable=goimports\
-  --enable=golint --min-confidence=0\
-  --enable=ineffassign\
-  --enable=interfacer\
-  --enable=lll --line-length=160\
-  --enable=megacheck\
-  --enable=misspell\
-  --enable=structcheck\
-  --enable=unconvert\
-  --enable=varcheck\
-  --enable=vet\
-  --enable=vetshadow\
-  --exclude='should have a package comment'\
-  --exclude=.pb.go\
-  --vendor\
-  --vendored-linters\
-  ./...
-echo 'gometalinter OK'
+function check_licences() {
+  echo 'Checking licences ...'
+  ${BIN_PATH}/check_license.sh
+  echo 'licences OK'
+}
 
-echo 'Checking licences ...'
-${BIN_PATH}/check_license.sh
-echo 'licences OK'
+function install_buildifier() {
+  echo 'Installing bazelbuilder ...'
+  go get -u github.com/bazelbuild/buildtools/buildifier
+}
 
-echo 'Running buildifier ...'
-go get github.com/bazelbuild/buildtools/buildifier
-buildifier -showlog -mode=check $(git ls-files| grep -e BUILD -e WORKSPACE | grep -v vendor)
-echo 'buildifier OK'
+function run_buildifier() {
+  "${GOPATH}/bin/buildifier" -showlog -mode=check $(git ls-files| grep -e BUILD -e WORKSPACE | grep -v vendor)
+  echo 'buildifier OK'
+}
+
+echo "GOPATH is set to ${GOPATH}"
+
+pushd ${ROOT}
+install_gometalinter
+run_gometalinter
+check_licences
+install_buildifier
+run_buildifier
+popd
