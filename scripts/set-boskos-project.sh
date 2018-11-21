c6748d0ac11fdbf60f40f26b407dacfa207435cc
Sebastien Vas <sebastienvas@google.com>
2018-11-21 11:31:16 -0800

    Fix project creation script

--- scripts/set-boskos-project.sh
+++ scripts/set-boskos-project.sh
@@ -26,18 +26,20 @@ SERVICES=(
 CREATE_PROJECT=false
 BILLING_ACCOUNT=
 
-while getopts :b:p:c arg; do
+while getopts :b:p:f:c arg; do
   case ${arg} in
     p) PROJECT_ID="${OPTARG}";;
     c) CREATE_PROJECT=true;;
     b) BILLING_ACCOUNT="${OPTARG}";;
+    f) FOLDER="${OPTARG}";;
     *) error_exit "Unrecognized argument -${OPTARG}";;
   esac
 done
 
 if [[ ${CREATE_PROJECT} == true ]]; then
   [[ -z "${BILLING_ACCOUNT}" ]] && { echo "use -b to set billing account"; exit 1; }
-  gcloud projects create "${PROJECT_ID}"
+  [[ -z "${FOLDER}" ]] && { echo "use -f to set folder"; exit 1; }
+  gcloud projects create --folder ${FOLDER} "${PROJECT_ID}"
   gcloud alpha billing projects link "${PROJECT_ID}" --billing-account "${BILLING_ACCOUNT}"
 fi
 
