4c654a682c9e939ea48f41a515e0ccd951e1040c
Christina Lyu <clyu@google.com>
2019-06-10 16:42:01 -0700

    jars/java

--- docker/istio/test-flakey/install.sh
+++ docker/istio/test-flakey/install.sh
@@ -17,12 +17,12 @@ apt-get -qqy --no-install-recommends install \
   unzip \
   wget
 
-./install-bazel.sh
+#./install-bazel.sh
 ./install-docker.sh
-./install-gcloud.sh
-./install-golang.sh
-./install-flakey.sh
-./install-javac.sh
+#./install-gcloud.sh
+#./install-golang.sh
+./install-java.sh
+./install-jars.sh
 
 apt-get clean
 rm -rf /var/lib/apt/lists/*
