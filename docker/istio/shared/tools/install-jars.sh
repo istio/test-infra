3af82b0ba593647f846b60ef00489d5f8d7d78e6
Christina Lyu <clyu@google.com>
2019-06-11 10:40:36 -0700

    new install-jars and readme

--- docker/istio/shared/tools/install-jars.sh
+++ docker/istio/shared/tools/install-jars.sh
@@ -2,9 +2,17 @@
 
 set -eux
 
+
 JARS_DIR="/usr/local/bin/flakey_jars"
-wget -q -nc -O jars.zip https://github.com/ChristinaLyu0710/istio-flakey-test/raw/master/FlakeyTest/flexible/Flakey/jars.zip
 mkdir -p "$JARS_DIR"
+wget -q -nc -O /usr/local/bin/flakey_jars/Pair.java https://raw.githubusercontent.com/istio/test-infra/master/flakeyTest/Pair.java
+wget -q -nc -O /usr/local/bin/flakey_jars/TotalFlakey.java https://raw.githubusercontent.com/istio/test-infra/master/flakeyTest/TotalFlakey.java
+wget -q -nc -O /usr/local/bin/flakey_jars/readPastJunitCommand.sh https://raw.githubusercontent.com/istio/test-infra/master/flakeyTest/readPastJunitCommand.sh
+wget -q -nc -O /usr/local/bin/flakey_jars/removeTempFolderCommand.sh https://raw.githubusercontent.com/istio/test-infra/master/flakeyTest/removeTempFolderCommand.sh
+
+wget -q -nc -O jars.zip https://github.com/ChristinaLyu0710/istio-flakey-test/raw/master/FlakeyTest/flexible/Flakey/jars.zip
+
+unzip flakeyTest.zip -d "$JARS_DIR"
 unzip jars.zip -d "$JARS_DIR"
 
 export PATH=${PATH}:/usr/local/bin
