df4156373c0c33201017ae0f32edb5d607c5fd73
Christina Lyu <clyu@google.com>
2019-06-11 10:46:14 -0700

    edit jars

--- docker/istio/shared/tools/install-jars.sh
+++ docker/istio/shared/tools/install-jars.sh
@@ -12,7 +12,6 @@ wget -q -nc -O /usr/local/bin/flakey_jars/removeTempFolderCommand.sh https://raw
 
 wget -q -nc -O jars.zip https://github.com/ChristinaLyu0710/istio-flakey-test/raw/master/FlakeyTest/flexible/Flakey/jars.zip
 
-unzip flakeyTest.zip -d "$JARS_DIR"
 unzip jars.zip -d "$JARS_DIR"
 
 export PATH=${PATH}:/usr/local/bin
