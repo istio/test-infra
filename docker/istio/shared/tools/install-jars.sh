7f5f880dbf29bb6a428eb563e5ee04537f4b16a9
Christina Lyu <clyu@google.com>
2019-06-11 11:53:11 -0700

    d

--- docker/istio/shared/tools/install-jars.sh
+++ docker/istio/shared/tools/install-jars.sh
@@ -7,4 +7,4 @@ wget -q -nc -O jars.zip https://github.com/ChristinaLyu0710/istio-flakey-test/ra
 mkdir -p "$JARS_DIR"
 unzip jars.zip -d "$JARS_DIR"
 
-export PATH=${PATH}:/usr/local/bin 
+export PATH=${PATH}:/usr/local/bin
