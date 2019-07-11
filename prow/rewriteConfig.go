84a3c436cf0b198c5c62ad3d99ab428c7b07e4c6
Christina Lyu <clyu@google.com>
2019-07-10 18:09:59 -0700

    fix minor changes

--- prow/rewriteConfig.go
+++ prow/rewriteConfig.go
@@ -1,3 +1,17 @@
+// Copyright 2018 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -167,7 +181,7 @@ func main() {
     newBranch := os.Args[1]
     filename := "config.yaml"
     
-    source, err = ioutil.ReadFile(filename)
+    source, err := ioutil.ReadFile(filename)
     if err != nil {
         log.Println("error when reading file")
         os.Exit(1)
@@ -176,7 +190,7 @@ func main() {
     resultString := findMaster(source, newBranch)
     resultBytes := []byte(resultString)
 
-    err := ioutil.WriteFile("config.yaml", resultBytes, 0600)
+    err = ioutil.WriteFile("config.yaml", resultBytes, 0600)
     if err != nil { 
         log.Println("error when writing file")
     }   
