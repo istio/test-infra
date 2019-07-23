a1751daac2ff137d498a658f9c5b70c45433a509
Christina Lyu <clyu@google.com>
2019-07-23 09:49:34 -0700

    split slice to read/write one section (100) files at a time

--- toolbox/errorfinder/main.go
+++ toolbox/errorfinder/main.go
@@ -105,6 +105,7 @@ func saveToken(path string, token *oauth2.Token) {
 	}
 }
 
+// Read google spreadsheets with credentials, spreadsheet ID and read range to get slice of file paths to build-logs.
 func readSpreadSheet(credentialsPath string, spreadsheetID string, readRange string) []string {
 	b, err := ioutil.ReadFile(credentialsPath)
 	if err != nil {
@@ -152,6 +153,7 @@ func NewErrorFinder(client *storage.Client, bucketName string) (*ErrorFinder, er
 	}, nil
 }
 
+// Read from gcs the build-log.txt files into slice of lines.
 func (f *ErrorFinder) query(ctx context.Context, prefix string) ([]string, error) {
 	client := f.client
 	bucket := client.Bucket(f.bucketName)
@@ -176,139 +178,251 @@ func (f *ErrorFinder) query(ctx context.Context, prefix string) ([]string, error
 	return lines, nil
 }
 
-func (f *ErrorFinder) getContent(ctx context.Context, filePaths []string) (map[string]string, map[string][]string, map[string][]string, error) {
-	errorMap := map[string][]string{}
-	warningMap := map[string][]string{}
-	contentMap := map[string]string{}
+// Get error contents of files to update error map and warning map with new errors/warnings and path to build logs.
+func (f *ErrorFinder) getContent(
+	ctx context.Context, filePaths []string, errorMap map[string][]string, warningMap map[string][]string) (map[string][]string, map[string][]string, error) {
 	for _, filePath := range filePaths {
 		fileSlice, err := f.query(ctx, filePath)
 		if err != nil {
 			continue
 		}
-
 		output := []string{}
 		for _, line := range fileSlice {
-			if !strings.Contains(line, "+") {
-				output = append(output, line)
+
+			// Ignore command lines to run that starts with "+".
+			if strings.Contains(line, "+") {
+				continue
 			}
+			// Generalize digits and content within quotations in current line.
+			generalized := f.generalizeDigits(line)
+			output = append(output, generalized)
 		}
-		outputString := strings.Join(output, "\n")
-		outputString = f.generalizeDigits(outputString)
 
-		processedString := outputString
-		errorString := ""
-		for {
-			ind := strings.Index(processedString, "exit status")
+		// Loop through only generalized result lines.
+		for _, line := range output {
+			toLower := strings.ToLower(line)
 
-			if ind == -1 {
-				break
-			}
-			splitSlice := strings.SplitAfterN(processedString, "exit status", 2)
-			beforeExit := strings.Split(splitSlice[0], "\n")
-			afterExit := strings.Split(splitSlice[1], "\n")
+			// Only look at lines with key words of "error", "warning" and "fail".
+			if strings.Contains(toLower, "error") || strings.Contains(toLower, "failed") || strings.Contains(toLower, "failure") || strings.Contains(toLower, "warn") {
 
-			var tenLinesBefore []string
-			if len(beforeExit) > 20 {
-				tenLinesBefore = beforeExit[len(beforeExit)-20:]
+				// Delete date information that are possibly present at the beginning of each line.
+				line = strings.TrimLeft(line, "\\d-T:.Z	info")
+				lineSplit := strings.Split(line, " ")
+				newLineSplit := []string{}
 
-			} else {
-				tenLinesBefore = beforeExit
-			}
+				for indexD, section := range lineSplit {
 
-			var tenLinesAfter []string
-			if len(afterExit) > 20 {
-				tenLinesAfter = afterExit[:20]
-			} else {
-				tenLinesAfter = afterExit[:]
-			}
-
-			total := append(tenLinesBefore, tenLinesAfter...)
-			errorLines := []string{}
-
-			for _, line := range total {
-				if strings.Contains(line, "error") || strings.Contains(line, "fail") {
-					line = strings.TrimLeft(line, "\\d-T:.Z	info")
-					lineSplit := strings.Split(line, " ")
-					newLineSplit := []string{}
-
-					for indexD, section := range lineSplit {
-						if strings.Compare(section, "") != 0 && !strings.Contains(section, "error") && !strings.Contains(section, "fail") {
-							if indexD > 0 && strings.Compare(lineSplit[indexD-1], "container") == 0 {
-								newLineSplit = append(newLineSplit, "\\s")
-							} else if strings.Contains(section, "\\d") {
-								newLineSplit = append(newLineSplit, "\\s")
-							} else if strings.Contains(section, "/") {
-								newLineSplit = append(newLineSplit, "\\s")
-							} else {
-								newLineSplit = append(newLineSplit, section)
-							}
-						} else if strings.Compare(section, "") != 0 {
-							newLineSplit = append(newLineSplit, section)
-						}
+					// Ignore empty strings
+					if strings.Compare(section, "") == 0 {
+						continue
 					}
 
-					line = strings.Join(newLineSplit, " ")
-					if strings.Contains(line, ":") {
-						indexColon := strings.Index(line, ":")
-						if indexColon < 20 {
-							newLine := strings.Replace(line, ":", "<", 1)
-							if strings.Contains(newLine, ":") {
-								indColon := strings.Index(newLine, ":")
+					// If word does not contain key words, generalize word to characters necessary.
+					if !strings.Contains(strings.ToLower(section), "error") &&
+						!strings.Contains(strings.ToLower(section), "fail") && !strings.Contains(strings.ToLower(section), "warn") {
+						if indexD > 0 && strings.Compare(lineSplit[indexD-1], "container") == 0 {
+							newLineSplit = append(newLineSplit, "\\s")
+						} else if strings.Contains(section, "\\d") {
+							newLineSplit = append(newLineSplit, "\\s")
+						} else if strings.Contains(section, "/") {
+							newLineSplit = append(newLineSplit, "\\s")
+						} else {
+							newLineSplit = append(newLineSplit, section)
+						}
+					} else {
+
+						// If word contains keyword, append it to list.
+						newLineSplit = append(newLineSplit, section)
+					}
+				}
+
+				// Get general error message not the detailed eplanation that usually comes after colon.
+				line = strings.Join(newLineSplit, " ")
+				if strings.Contains(line, ":") {
+					indexColon := strings.Index(line, ":")
+					if indexColon < 20 {
+						newLine := strings.Replace(line, ":", "<", 1)
+						if strings.Contains(newLine, ":") {
+							indColon := strings.Index(newLine, ":")
+							if strings.Contains(line[:indColon], "error") || strings.Contains(line[:indColon], "warn") || strings.Contains(line[:indColon], "fail") {
 								line = line[:indColon]
 							}
-						} else {
+
+						}
+					} else {
+						if strings.Contains(line[:indexColon], "error") || strings.Contains(line[:indexColon], "warn") || strings.Contains(line[:indexColon], "fail") {
 							line = line[:indexColon]
 						}
 					}
-					errorLines = append(errorLines, line)
+				}
 
-					if strings.Contains(line, "warn") {
-						var warningPrs []string
-						if warningMap[line] == nil {
-							warningPrs = []string{filePath}
-						} else {
-							warningPrs = warningMap[line]
-							if !contains(warningPrs, filePath) {
-								warningPrs = append(warningPrs, filePath)
-							}
-						}
-						warningMap[line] = warningPrs
+				// If error line contains keyword "warn", it is a warning and should be placed in warning map.
+				if strings.Contains(strings.ToLower(line), "warn") {
+					var warningPrs []string
+					if warningMap[line] == nil {
+						warningPrs = []string{filePath}
 					} else {
-						var prSlice []string
-						if errorMap[line] == nil {
-							prSlice = []string{filePath}
-						} else {
-							prSlice = errorMap[line]
-							if !contains(prSlice, filePath) {
-								prSlice = append(prSlice, filePath)
-							}
+						warningPrs = warningMap[line]
+						if !contains(warningPrs, filePath) {
+							warningPrs = append(warningPrs, filePath)
 						}
-						errorMap[line] = prSlice
 					}
+					warningMap[line] = warningPrs
+					// Else it's an error and should be placed to error map.
+				} else {
+					var prSlice []string
+					if errorMap[line] == nil {
+						prSlice = []string{filePath}
+					} else {
+						prSlice = errorMap[line]
+						if !contains(prSlice, filePath) {
+							prSlice = append(prSlice, filePath)
+						}
+					}
+					errorMap[line] = prSlice
 				}
 			}
-
-			errorString = errorString + "\n" + strings.Join(errorLines, "\n")
-			processedString = splitSlice[1]
 		}
-		contentMap[filePath] = errorString
 	}
-	return contentMap, errorMap, warningMap, nil
+	return errorMap, warningMap, nil
 }
 
+// Read destination csv file to get previously generated errors and warnings.
+func readCSV(fileName string) (map[string][]string, map[string][]string, error) {
+	existingError := map[string][]string{}
+	warningMap := map[string][]string{}
+	csvFile, err := os.Open(fileName)
+	if err != nil {
+		return existingError, warningMap, err
+	}
+
+	var isError bool
+
+	// Read csv file with bufio and scanner line by line.
+	scanner := bufio.NewScanner(csvFile)
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), ",")
+
+		// If header "error" is found, the following lines are all errors that should
+		// be stored in map existingError.
+		if strings.Compare(line[0], "error") == 0 {
+			isError = true
+			continue
+		}
+
+		if isError {
+
+			// If header "warning" is found, the follwing lines after "warning" are
+			// all warnings that should be stored in map warningMap.
+			if strings.Compare(line[0], "warning") == 0 {
+				isError = false
+				continue
+			} else {
+				errorMessage := line[0]
+				ind := 2
+				for {
+					ele := line[ind]
+					if strings.Contains(ele, "/") {
+						break
+					}
+					ind++
+				}
+				errorAppearance := line[ind:]
+				existingError[errorMessage] = errorAppearance
+			}
+			// If the lines are warnings, update the warning map.
+		} else {
+			warningMessage := line[0]
+			ind := 2
+			for {
+				ele := line[ind]
+				if strings.Contains(ele, "/") {
+					break
+				}
+				ind++
+			}
+
+			warningAppearance := line[ind:]
+			warningMap[warningMessage] = warningAppearance
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return existingError, warningMap, nil
+}
+
+// For each spliter section of file paths, read previously generated errors and warnings.
+// Process contents in new files and update the csv.
+func (f *ErrorFinder) findErrorForEachSection(ctx context.Context, gcsFilePaths []string, OutputFileName string, startInd int, endInd int) {
+	curErrors, curWarnings, err := readCSV(OutputFileName)
+
+	// If csv file does not contain anything yet, initialize curErrors and curWarnings to be empty maps.
+	if err != nil {
+		curErrors = map[string][]string{}
+		curWarnings = map[string][]string{}
+	}
+	filesToProcess := append([]string{}, gcsFilePaths[startInd:endInd]...)
+
+	// Get errors and warnings from newly read build logs and update maps of errors and warnings.
+	newErrors, newWarnings, err := f.getContent(ctx, filesToProcess, curErrors, curWarnings)
+
+	if err != nil {
+		return
+	}
+
+	// Write new errors and warnings to output csv file.
+	writeCSV(newErrors, newWarnings, OutputFileName)
+}
+
+// Divide the list of file paths read in gcs storage to several spliters to avoid overflooding memory.
+func (f *ErrorFinder) divideToSections(ctx context.Context, spliter int, gcsFilePaths []string, OutputFileName string) {
+	n := 1
+	for {
+		if n*spliter > len(gcsFilePaths) {
+			break
+		}
+		startInd := (n - 1) * spliter
+		endInd := n*spliter - 1
+		f.findErrorForEachSection(ctx, gcsFilePaths, OutputFileName, startInd, endInd)
+		n++
+	}
+	startInd := (n - 1) * spliter
+	endInd := len(gcsFilePaths)
+	f.findErrorForEachSection(ctx, gcsFilePaths, OutputFileName, startInd, endInd)
+}
+
+// Generalize messages from build file.
 func (f *ErrorFinder) generalizeDigits(content string) string {
+	// Generalize unique numbers in build-log.txt to character '\d'
 	reg, _ := regexp.Compile("[0-9]+")
 	newContent := reg.ReplaceAllString(content, "\\d")
+
+	// Generalize unique messages in build-log.txt surrounded by quotations to character '\d'
 	reg, _ = regexp.Compile("\\\"(.*?)\\\"|'(.*?)'")
 	newContent = reg.ReplaceAllString(newContent, "\\s")
 
 	return newContent
 }
 
+// Write error map and warning map to csv file with given file path.
 func writeCSV(errorMap map[string][]string, warningMap map[string][]string, fileName string) {
-	file, err := os.Create(fileName)
-	if err != nil {
-		log.Fatal("cannot open file", err)
+	var file *os.File
+	var err error
+	// If file already exists in the path, write to original file.
+	if _, err := os.Stat(fileName); err == nil {
+		file, err = os.OpenFile(fileName, os.O_RDWR, 0644)
+		if err != nil {
+			log.Fatal("cannot open file", err)
+		}
+		// If file does not exist, create a new file to write to.
+	} else {
+		file, err = os.Create(fileName)
+		if err != nil {
+			log.Fatal("cannot open file", err)
+		}
 	}
 
 	defer file.Close()
@@ -320,6 +434,8 @@ func writeCSV(errorMap map[string][]string, warningMap map[string][]string, file
 	if err != nil {
 		log.Fatal("cannot write line", err)
 	}
+
+	// Write each line of csv file for error map with a error header.
 	for key, value := range errorMap {
 		newLine := []string{}
 		newLine = append(newLine, key)
@@ -332,6 +448,8 @@ func writeCSV(errorMap map[string][]string, warningMap map[string][]string, file
 		}
 
 	}
+
+	// Write each line of csv file for warning map with warning header.
 	warning := []string{"warning"}
 	err = writer.Write(warning)
 	if err != nil {
@@ -353,15 +471,15 @@ func writeCSV(errorMap map[string][]string, warningMap map[string][]string, file
 
 func main() {
 	var fileName string
-	flag.StringVar(&fileName, "OutputFileName", "", "A file name for output csv file with .csv at the end.")
+	flag.StringVar(&fileName, "OutputFileName", "output.csv", "A file name for output csv file with .csv at the end.")
 	var spreadsheetID string
-	flag.StringVar(&spreadsheetID, "SpreadsheetID", "", "Spreadsheet ID for the spreadsheet containing file path")
+	flag.StringVar(&spreadsheetID, "SpreadsheetID", "1XK4Nbq7nwqwXMC7waMsEqhe_wOf1J1rqwbECrop9fQI", "Spreadsheet ID for the spreadsheet containing file path")
 	var credentialsPath string
-	flag.StringVar(&credentialsPath, "CredentialsPath", "", "Path to Credentials.json file.")
+	flag.StringVar(&credentialsPath, "CredentialsPath", "credentials.json", "Path to Credentials.json file.")
 	var readRange string
-	flag.StringVar(&readRange, "ReadRange", "", "ReadRange from the spread sheet such as Sheet1!A1:A reads all elements in column A sheet 1.")
+	flag.StringVar(&readRange, "ReadRange", "A1:A5", "ReadRange from the spread sheet such as Sheet1!A1:A reads all elements in column A sheet 1.")
 	var bucketName string
-	flag.StringVar(&bucketName, "BucketName", "", "Bucket Name to read from in GCS.")
+	flag.StringVar(&bucketName, "BucketName", "istio-prow", "Bucket Name to read from in GCS.")
 
 	flag.Parse()
 
@@ -396,10 +514,5 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to create new error finder: %v", err)
 	}
-
-	_, errorMap, warningMap, err := errorFinder.getContent(context, listOfPrs)
-	if err != nil {
-		fmt.Println(err)
-	}
-	writeCSV(errorMap, warningMap, fileName)
+	errorFinder.divideToSections(context, 2, listOfPrs, fileName)
 }
