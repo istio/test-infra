eee87fecd42dc3c4374494a319b444f034cb2ea7
Piotr Sikora <piotrsikora@google.com>
2019-08-18 03:43:31 +0000

    Use Bazelisk launcher for Bazel.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- docker/istio/shared/tools/install-bazel.sh
+++ docker/istio/shared/tools/install-bazel.sh
@@ -2,12 +2,12 @@
 
 set -eux
 
-BAZEL_VERSION='0.28.0'
-BAZEL_BASE_URL='https://github.com/bazelbuild/bazel/releases/download'
-BAZEL_SH="bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
-BAZEL_URL="${BAZEL_BASE_URL}/${BAZEL_VERSION}/${BAZEL_SH}"
+# Use Bazelisk launcher for Bazel (automatically downloads required version of Bazel).
+BAZELISK_VERSION="v1.0"
+BAZELISK_BASE_URL="https://github.com/bazelbuild/bazelisk/releases/download"
+BAZELISK_BIN="bazelisk-linux-amd64"
+BAZELISK_URL="${BAZELISK_BASE_URL}/${BAZELISK_VERSION}/${BAZELISK_BIN}"
 
-wget -q -nc "${BAZEL_URL}"
-chmod +x "${BAZEL_SH}"
-./${BAZEL_SH}
-rm -rf "${BAZEL_SH}"
+wget -q -nc "${BAZELISK_URL}"
+chmod +x "${BAZELISK_BIN}"
+mv ./${BAZELISK_BIN} /usr/local/bin/bazel
