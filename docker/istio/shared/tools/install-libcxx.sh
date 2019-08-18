bcd768416e2f768cd5584773e505b5bf64660ea1
Piotr Sikora <piotrsikora@google.com>
2019-08-18 01:54:21 +0000

    review: update messages to mention libc++.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- docker/istio/shared/tools/install-libcxx.sh
+++ docker/istio/shared/tools/install-libcxx.sh
@@ -7,8 +7,8 @@ LLVM_REPO='xenial'
 
 echo "deb http://apt.llvm.org/${LLVM_REPO}/ llvm-toolchain-${LLVM_REPO}-${LLVM_VERSION} main" \
     | tee /etc/apt/sources.list.d/llvm.list
-echo 'Adding repo for llvm'
+echo 'Adding repo for libc++'
 curl https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
-echo 'Installing clang'
+echo 'Installing libc++'
 apt-get update
 apt-get -qqy install "libc++-${LLVM_VERSION}-dev" "libc++abi-${LLVM_VERSION}-dev"
