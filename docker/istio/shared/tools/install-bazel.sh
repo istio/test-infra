c982ea7773ef73e751aa0bfadcbcdaf1dd6b0470
Piotr Sikora <piotrsikora@google.com>
2019-08-19 22:13:55 +0000

    review: preload images with known Bazel versions.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- docker/istio/shared/tools/install-bazel.sh
+++ docker/istio/shared/tools/install-bazel.sh
@@ -11,3 +11,8 @@ BAZELISK_URL="${BAZELISK_BASE_URL}/${BAZELISK_VERSION}/${BAZELISK_BIN}"
 wget -q -nc "${BAZELISK_URL}"
 chmod +x "${BAZELISK_BIN}"
 mv ./${BAZELISK_BIN} /usr/local/bin/bazel
+
+# Preload Bazel versions that we know are needed.
+USE_BAZEL_VERSION=0.22.0 /usr/local/bin/bazel version # release-1.1
+USE_BAZEL_VERSION=0.26.1 /usr/local/bin/bazel version # release-1.2
+USE_BAZEL_VERSION=0.28.1 /usr/local/bin/bazel version # release-1.3
