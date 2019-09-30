e74573ad843a065d91d2b7eec266be9a76dd1183
John Howard <howardjohn@google.com>
2019-09-30 11:18:24 -0700

    Don't use custom trigger command (#1864)
    * Don't use custom trigger command
    
    Apparently this doesn't work and is not recommened
    
    Fixes https://github.com/istio/test-infra/issues/1833
    
    * Fix lint

--- prow/config/generate.go
+++ prow/config/generate.go
@@ -157,11 +157,6 @@ func ValidateJobConfig(jobConfig JobConfig) {
 	}
 }
 
-func triggerRegex(job string) string {
-	// This is based on the default regex for jobs, but replacing the full job name with the short job name
-	return fmt.Sprintf(`(?m)^/test (?:.*? )?%s(?: .*?)?$`, job)
-}
-
 func ConvertJobConfig(jobConfig JobConfig, branch string) config.JobConfig {
 	var presubmits []config.Presubmit
 	var postsubmits []config.Postsubmit
@@ -192,11 +187,9 @@ func ConvertJobConfig(jobConfig JobConfig, branch string) config.JobConfig {
 			}
 
 			presubmit := config.Presubmit{
-				JobBase:      createJobBase(jobConfig, job, name, jobConfig.Repo, branch, jobConfig.Resources),
-				AlwaysRun:    true,
-				Brancher:     brancher,
-				Trigger:      triggerRegex(job.Name),
-				RerunCommand: fmt.Sprintf(`/test %s`, job.Name),
+				JobBase:   createJobBase(jobConfig, job, name, jobConfig.Repo, branch, jobConfig.Resources),
+				AlwaysRun: true,
+				Brancher:  brancher,
 			}
 			presubmit.JobBase.Annotations[TestGridDashboard] = testgridJobPrefix
 			applyModifiersPresubmit(&presubmit, job.Modifiers)
