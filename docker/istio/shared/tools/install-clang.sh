b5496e7faa23917f49c49fe17b4d9144276a1663
Piotr Sikora <piotrsikora@google.com>
2019-10-17 20:15:52 +0000

    Update clang, libc++, and Bazel in prowbazel images.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- docker/istio/shared/tools/install-clang.sh
+++ docker/istio/shared/tools/install-clang.sh
@@ -16,7 +16,7 @@
 
 set -eux
 
-LLVM_VERSION=8
+LLVM_VERSION=9
 LLVM_REPO='xenial'
 
 echo "deb http://apt.llvm.org/${LLVM_REPO}/ llvm-toolchain-${LLVM_REPO}-${LLVM_VERSION} main" \
@@ -26,4 +26,3 @@ curl https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 echo 'Installing clang'
 apt-get update
 apt-get -qqy install "clang-${LLVM_VERSION}" "clang-format-${LLVM_VERSION}" "clang-tidy-${LLVM_VERSION}" "lld-${LLVM_VERSION}" "libc++-${LLVM_VERSION}-dev" "libc++abi-${LLVM_VERSION}-dev"
-
