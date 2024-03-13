491d2269bda1a63ca499a3a4ac2f4c727e62b2d2
John Howard <howardjohn@google.com>
2024-03-13 13:56:40 -0700

    move another to modules

--- infra/gcp/istio-prow-build/cluster-prow-arm.tf
+++ infra/gcp/istio-prow-build/cluster-prow-arm.tf
@@ -85,52 +85,21 @@ resource "google_container_cluster" "prow_arm" {
 
 # The default pool hosts an x86 node pool. This is to run some of the prow infrastructure which isn't arm compatible.
 # This is just a single node without scaling, no tests run here.
-resource "google_container_node_pool" "prow_arm_default" {
-  cluster            = "prow-arm"
-  initial_node_count = 1
-  location           = "us-central1-f"
+module "prow_arm_default" {
+  source       = "../modules/gke-nodepool"
+  name         = "default-pool"
+  project_name = "istio-prow-build"
+  location     = "us-central1-f"
+  cluster_name = "prow-arm"
 
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
+  initial_count = 1
+  min_count     = 1
+  max_count     = 1
 
-  name = "default-pool"
-
-  node_config {
-    disk_size_gb = 100
-    disk_type    = "pd-ssd"
-    image_type   = "COS_CONTAINERD"
-    machine_type = "n1-standard-8"
-
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring", "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"
-    ]
-    service_account = "default"
-
-    shielded_instance_config {
-      enable_integrity_monitoring = true
-    }
-
-    workload_metadata_config {
-      mode = "GKE_METADATA"
-    }
-  }
-
-  node_count     = 1
-  node_locations = ["us-central1-f"]
-  project        = "istio-prow-build"
-
-  upgrade_settings {
-    max_surge       = 1
-    max_unavailable = 0
-  }
+  disk_size_gb    = 100
+  disk_type       = "pd-ssd"
+  machine_type    = "n1-standard-8"
+  service_account = "istio-prow-jobs@istio-prow-build.iam.gserviceaccount.com"
 }
 
 # This pool provides the actual ARM (t2a) instances for tests.
@@ -149,14 +118,14 @@ module "prow_arm_test_spot" {
 
   disk_size_gb = 256
   disk_type    = "pd-ssd"
-  labels       = {
+  labels = {
     testing = "test-pool"
   }
 
   arm          = true
   machine_type = "t2a-standard-16"
   # Spot instances are used as quota is capped for ARM nodes, and its cheaper.
-  spot         = true
+  spot = true
 
   service_account = "istio-prow-jobs@istio-prow-build.iam.gserviceaccount.com"
 
