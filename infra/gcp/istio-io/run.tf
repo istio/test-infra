b2760201e3bcc8f9a473c70f4e57e4c0082bf94e
Craig Box <craig.box@gmail.com>
2024-08-07 13:13:41 +1200

    Another new Slack URL

--- infra/gcp/istio-io/run.tf
+++ infra/gcp/istio-io/run.tf
@@ -18,8 +18,8 @@ resource "google_cloud_run_v2_service" "redirector" {
       image = "gcr.io/istio-testing/redirector"
       env {
         name = "REDIRECT_URL"
-        # expires 2034-07-21 or after 400 uses
-        value = "https://join.slack.com/t/istio/shared_invite/zt-2n6a8whhg-8IB9cGVezfL5Kqyy9Dfbxg"
+        # expires 2024-07-30 or after 2000 uses
+        value = "https://join.slack.com/t/istio/shared_invite/zt-2nnma2nk5-ed6E8~5LVowTzF48z~MNug"
       }
       resources {
         cpu_idle = true
